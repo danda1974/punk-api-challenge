f6b74c7f4a84af486815f5df9f2ca72f0d11362e
Daniela Spacek <familie.spacek@googlemail.com>
2022-05-17 17:45:27 +0200

    minor changes

--- src/components/NavBar/SearchBox/SearchBox.jsx
+++ src/components/NavBar/SearchBox/SearchBox.jsx
@@ -1,9 +1,14 @@
 import "./SearchBox.scss";
 
-const SearchBox = () => {
+const SearchBox = ({ searchTerm, handleInput }) => {
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const handleInput = event => {
+  //   const cleanInput = event.target.value.toLowerCase();
+  //   setSearchTerm(cleanInput);
+  // };
   return (
     <div className="searchBox">
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." value={searchTerm} onInput={handleInput} className="search-box__input"/>
     </div>
   );
 };
